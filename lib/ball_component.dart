db53f3a5805dc0c09a2aa30dc0977ca159804e4f
zhafarullah <ahmadzhafarullah@gmail.com>
2024-11-01 17:04:07 +0700

    tambahan pojok bola

--- lib/ball_component.dart
+++ lib/ball_component.dart
@@ -10,7 +10,7 @@ class BallComponent extends PositionComponent {
 
   @override
   void render(Canvas canvas) {
-    final paint = BasicPalette.blue.paint(); // Warna bola
+    final paint = BasicPalette.green.paint(); // Warna bola
     canvas.drawCircle(Offset(size.x / 2, size.y / 2), size.x / 2, paint);
   }
 }
