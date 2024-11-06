60fb8a4110553ced76176fd81afdaea61b81d2a6
Athaboy <rafifi0607@gmail.com>
2024-11-06 17:12:10 +0700

    update : test sensor cahaya

--- grand_ball_auto/test/widget_test.dart
+++ grand_ball_auto/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:grand_ball_auto/main.dart';
+import 'package:grand_ball_auto/maintttt.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
